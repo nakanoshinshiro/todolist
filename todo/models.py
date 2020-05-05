a7b656bc4b90a0468e3130311f4cc6e68ee679e0
shinshiro <bloodymindedness@gmail.com>
2020-05-05 14:02:13 +0900

    Todo List app, first commit

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+PRIORITY = (("danger","高"),("info","中"),("success","低"))
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100,verbose_name="タイトル")
+    memo = models.TextField(verbose_name="メモ")
+    priority = models.CharField(
+        max_length=10,choices=PRIORITY,verbose_name="優先度")
+    duedate = models.DateField(verbose_name="期限")
+    def __str__(self):
+        return self.title
